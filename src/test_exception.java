f022073e1af011a62c98867f245086d853e6bb34
HP <HP@LAPTOP-9GM69S8D.lan>
2019-04-21 10:49:57 +0800

    hello last

--- src/test_exception.java
+++ src/test_exception.java
@@ -1,5 +1,10 @@
 class MyException extends Exception{
-
+	 public MyException(String message) {
+		   super(message);
+	 }
+	 public String getMessage() {
+		 return String.format("\"%s\"", Thread.currentThread().getName())+" For input string :"+super.getMessage();
+	 }
 }
 class MyTool{
 	 public static int Parseint(String str) throws MyException{
@@ -9,7 +14,7 @@ class MyTool{
 			 char ch = str.charAt(i);
 			 if(ch>='0'&&ch<='9')
 			    result = result*10+ch-'0';
-			 else throw new MyException();
+			 else throw new MyException(str);
 	   
 	     }
 		 return result;
@@ -20,11 +25,11 @@ public class test_exception {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 try {
-			System.out.println(MyTool.Parseint("345s"));
+			System.out.println(MyTool.Parseint("123L"));
 		 }
 		 catch(MyException e){
-			  System.out.println("×Ö·û´®×ª»»Òì³£");
-			 
+			  //System.out.println("×Ö·û´®×ª»»Òì³£");
+			 e.printStackTrace();
 			 
 		 }
 		
